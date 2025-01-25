43248c58d1f8b1f9b4fd3c33abb5eda85f09db8a
Muhammad Farooq <farooqayub646@gmail.com>
2025-01-25 14:48:08 +0500

    updated code

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="w-full min-h-screen h-full scrollbar-0">
-      <SidebarTrigger />
+      {/* <SidebarTrigger /> */}
       <InitialView />
     </div>
   );
